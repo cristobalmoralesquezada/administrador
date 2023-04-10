ffcf30864adaf253682743151b2af9b76a997b04
cristobal <moralesquezada@gmail.com>
2023-04-10 18:11:13 -0400

    iniciando Menú en dashboard

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  items: MenuItem[] = [];
+
+  ngOninit(): void{
+    this.items = [
+      {
+        label: 'File',
+        items: [{
+          label: 'New',
+          icon: 'pi pi-fw pi-plus',
+          items: [
+            {label: 'Project'},
+            {label: 'Other'},
+          ]},
+        {label: 'Open'},
+        {label: 'Quit'},
+      ],
+      }
+    ]
+  }
+
+}
